6e1e88a78b751ba61493406e0e0eb4f550fce409
Siddant Rachha <siddant.rachha@gmail.com>
2025-02-23 01:41:53 +0530

    implement workspace structure; create a demo surface; create a demo microfrontend; consume microfrontend in surface

---
+++ demo-surface/src/app/page.tsx
@@ -0,0 +1,9 @@
+import {MFButton} from 'demo-microfrontend';
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <MFButton text='This text comes from suface' />
+    </div>
+  );
+}
